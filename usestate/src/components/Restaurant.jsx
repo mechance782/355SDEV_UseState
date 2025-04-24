fd7e5c42a7f95adcd71982eff641d6cfd389c590
Madeleine Chance <mechance782@gmail.com>
2025-04-23 17:27:27 -0700

    completed state learning practice

--- usestate/src/components/Restaurant.jsx
+++ usestate/src/components/Restaurant.jsx
@@ -1,4 +1,36 @@
+import { useState } from "react";
+
 function Restaurant(props) {
+  const [ toggleHours, setToggleHours] = useState(false);
+  const [ toggleMenu, setToggleMenu ] = useState(false);
+
+  const handleMenuButton = () => setToggleMenu(prevState => !prevState);
+  const handleHoursButton = () => setToggleHours(prevState => !prevState);
+
+  function Menu({ menu }){
+    return(
+      <ul>
+        {menu.map(food => (
+          <li>{food.item} - ${food.price}</li>
+        ))}
+      </ul>
+    )
+  }
+
+  function Hours({ hours }){
+    return(
+      <ul>
+        {
+          Object.entries(hours).map(([day, time]) => (
+            <li>
+              <span style={{fontWeight:"bold"}}>{day.charAt(0).toUpperCase()}{day.slice(1)}:</span> {time}
+            </li>
+          ))
+        }
+      </ul>
+    )
+  }
+
   return (
     <div className="restaurant">
       <img src={props.restaurant.image} alt={props.name} />
@@ -9,8 +41,10 @@ function Restaurant(props) {
         <li>{props.restaurant.cuisine}</li>
         <li>{props.restaurant.rating}</li>
       </ul>
-      <button>Hours</button>
-      <button>Menu</button>
+      <button onClick={handleHoursButton}>Hours</button>
+      <button onClick={handleMenuButton}>Menu</button>
+      {toggleMenu && Menu(props.restaurant)}
+      {toggleHours && Hours(props.restaurant)}
     </div>
   );
 }
